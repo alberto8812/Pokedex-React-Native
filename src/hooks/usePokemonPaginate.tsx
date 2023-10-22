601424cc410a5165495e4c92d09e64334f3fdbf6
alberto8812 <cvelascosaavedra@gmail.com>
2023-10-22 18:33:57 -0500

    create flat list  with infinity scroll

--- src/hooks/usePokemonPaginate.tsx
+++ src/hooks/usePokemonPaginate.tsx
@@ -37,6 +37,7 @@ const mapPokemonList=(pokemonList:Result[])=>{
    return{
     isLoading,
     simplePokemonList,
+    loadPokemons,
    }
    
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ActivityIndicator, Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { Image } from 'react-native'
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { usePokemonPaginate } from '../hooks/usePokemonPaginate';
+import { FlatList } from 'react-native';
 
 
 export const HomeScreen = () => {
  const {top}=useSafeAreaInsets();
- const {isLoading,simplePokemonList}=usePokemonPaginate();
+ const {isLoading,simplePokemonList,loadPokemons}=usePokemonPaginate();
   return (
     <>
         <Image
@@ -25,6 +26,30 @@ export const HomeScreen = () => {
         >
             Pokedex
         </Text>
+
+        <FlatList
+        
+        data={simplePokemonList}
+        keyExtractor={(pokemon)=>pokemon.id}
+        renderItem={({item})=><Text>{item.name}</Text>}
+        showsVerticalScrollIndicator={false}
+
+      //infinite scroll
+      onEndReached={loadPokemons} //when is near finally call loadPokemons
+      onEndReachedThreshold={0.4}
+
+      ListFooterComponent={
+       ( <ActivityIndicator  
+        style={{height:100}}
+        color='grey'
+        size={20}
+        />)}
+
+        
+        />
+
+
+
     </>
   )
 }
