b35e18e9503f3e781955278a467b7e07ec020bd0
alberto8812 <cvelascosaavedra@gmail.com>
2023-10-22 17:58:16 -0500

    construction interfaces api pokemon

--- src/hooks/usePokemonPaginate.tsx
+++ src/hooks/usePokemonPaginate.tsx
@@ -1,18 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { pokemonApi } from "../api/pokemonApi";
+import { PokemonPaginateResponse, Result, SimplePokemon } from "../interfaces/pokemonInterfaces";
 
 export const usePokemonPaginate = () => {
    const url=`https://pokeapi.co/api/v2/pokemon?limit=40`;
 
+   const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
    const nextPageUrl = useRef(`https://pokeapi.co/api/v2/pokemon?limit=40`);
 
    const loadPokemons=async()=>{
-        const {data}=await pokemonApi.get(nextPageUrl.current);
-
-        console.log(data)
+        const {data}=await pokemonApi.get<PokemonPaginateResponse>(nextPageUrl.current);
+        nextPageUrl.current=data.next;
+        mapPokemonList(data.results)
        };
 
-
+   const mapPokemonList=(pokemonList:Result[])=>{
+       pokemonList.forEach(poke=>console.log(poke.name));
+   }
    useEffect(() => {
 
     loadPokemons();
@@ -20,7 +24,7 @@ export const usePokemonPaginate = () => {
    }, [])
 
    return{
-
+    simplePokemonList,
    }
    
 }
