8c838275960cad1718185d1944b20315ece5da21
alberto8812 <cvelascosaavedra@gmail.com>
2023-10-24 10:21:50 -0500

    desing search component

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,7 +1,9 @@
+export { SearchInput } from "../components/SearchInput";
 export { SearchScreen } from "./SearchScreen";
 export { UserConfiguration } from "./UserConfiguration";
 export { HomeScreen } from "./HomeScreen";
 export { PokemonScreen } from "./PokemonScreen";
+
 
 
 
